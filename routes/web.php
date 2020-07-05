c0defa04199bea904ae8707e0d798b7600b16a21
qspc <shibgotalloh_qqq@apps.ipb.ac.id>
2020-07-05 19:07:36 +0700

    done

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,17 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('artikel.home');
 });
+Route::get('/artikel/create', 'ArtikelController@create');
+Route::post('/artikel', 'ArtikelController@store');
+Route::get('/artikel', 'ArtikelController@index');
+Route::get('/artikel/{artikel_id}', 'ArtikelController@show');
+Route::delete('/artikel/{artikel_id}', 'ArtikelController@destroy');
+Route::get('/artikel/{artikel_id}/edit', 'ArtikelController@edit');
+Route::put('/artikel/{artikel_id}', 'ArtikelController@update');
 
+/*
 Route::get('/items/create', 'ItemController@create'); // menampilkan halaman form
 Route::post('/items', 'ItemController@store'); // menyimpan data
 Route::get('/items', 'ItemController@index'); // menampilkan semua
@@ -22,3 +30,4 @@ Route::get('/items/{id}', 'ItemController@show'); // menampilkan detail item den
 Route::get('/items/{id}/edit', 'ItemController@edit'); // menampilkan form untuk edit item
 Route::put('/items/{id}', 'ItemController@update'); // menyimpan perubahan dari form edit
 Route::delete('/items/{id}', 'ItemController@destroy'); // menghapus data dengan id
+*/
